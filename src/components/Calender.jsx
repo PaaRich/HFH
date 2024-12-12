885e89243da50cd5d13d227f1b495863899aa16b
PaaRich <siehrichmond50@gmail.com>
2024-12-12 04:30:10 +0000

    calender

--- src/components/Calender.jsx
+++ src/components/Calender.jsx
@@ -1,5 +1,21 @@
+import { useState, useCallback } from "react";
+import { Calendar } from "@natscale/react-calendar";
+
 const Calender = () => {
-  return <div>Calender</div>;
+  const [value, setValue] = useState();
+
+  const onChange = useCallback(
+    (value) => {
+      setValue(value);
+    },
+    [setValue]
+  );
+
+  return (
+    <div className="w-full">
+      <Calendar value={value} onChange={onChange} useDarkMode />
+    </div>
+  );
 };
 
 export default Calender;
