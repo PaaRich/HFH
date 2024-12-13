39c8f39cf7910194c917f883ffaa1262b2127379
PaaRich <siehrichmond50@gmail.com>
2024-12-13 07:18:14 +0000

    handle side hide settimeout and outside click

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect } from "react";
 import SideBar from "./SideBar";
 //import Navbar from "./Navbar";
 import { Outlet } from "react-router-dom";
@@ -7,11 +7,33 @@ import { AiOutlineCaretRight } from "react-icons/ai";
 
 const Layout = () => {
   const { open, setIsOpen } = useContext(Context);
+  const SidebarRef = useRef(null);
+
+  useEffect(() => {
+    // const handleOutsideClick = (event) => {
+    //   if (!SidebarRef.current.contains(event.target)) {
+    //     setIsOpen(false);
+    //   }
+    // };
+    const handleTimeOutHide = () => {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 10000);
+    };
+
+    document.addEventListener("mousedown", handleTimeOutHide);
+
+    return () => {
+      document.removeEventListener("mousedown", handleTimeOutHide);
+    };
+  }, [SidebarRef]);
+
   return (
     <main className="h-dvh">
       <div className="md:flex">
         {/* sidebar */}
         <div
+          ref={SidebarRef}
           className={`c-sidebar md:w-[5%] ${
             open ? "left-0" : "-left-[5%]"
           } duration-150 border-r-[1px] fixed z-50 top-0`}
@@ -21,7 +43,7 @@ const Layout = () => {
             className={`${
               open
                 ? "hover:scale-110 duration-150"
-                : "hover:scale-110 duration-200"
+                : "hover:scale-110 hover:translate-x-1 duration-200"
             } absolute -right-6 top-1/2 cursor-pointer `}
             onClick={() => setIsOpen(!open)}
           >
