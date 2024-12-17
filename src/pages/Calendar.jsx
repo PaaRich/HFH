af6ca3e6002a7fb4ab5964713ff21fbf4d7b9e73
PaaRich <siehrichmond50@gmail.com>
2024-12-17 00:55:56 +0000

    implemented badge update

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { Context } from "../Context/Context";
+
 const Calendar = () => {
-  return <div>Calendar</div>;
+  const { setCalendar } = useContext(Context);
+  return (
+    <div className="flex items-center justify-center w-full">
+      <button onClick={() => setCalendar(false)} className="border-2 ">
+        Toggle Calendar
+      </button>
+    </div>
+  );
 };
 
 export default Calendar;
