91f103a9adc3d89a1882b6028c6b4b8fd272f9fb
PaaRich <siehrichmond50@gmail.com>
2024-12-17 23:46:34 +0000

    renamed context variables

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -4,20 +4,20 @@ import { useState } from "react";
 // eslint-disable-next-line react/prop-types
 const AppContext = ({ children }) => {
   const [open, setIsOpen] = useState(false);
-  const [meeting, setMeeting] = useState(true);
-  const [calendar, setCalendar] = useState(true);
-  const [notification, setNotification] = useState(true);
+  // const [meeting, setMeeting] = useState(true);
+  const [schedule, setSchedule] = useState(true);
+  const [event, setEvent] = useState(true);
   return (
     <Context.Provider
       value={{
         open,
         setIsOpen,
-        meeting,
-        setMeeting,
-        calendar,
-        setCalendar,
-        notification,
-        setNotification,
+        //meeting,
+        //setMeeting,
+        schedule,
+        setSchedule,
+        event,
+        setEvent,
       }}
     >
       {children}
