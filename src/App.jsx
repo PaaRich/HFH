7ed5ff7c89e04614c1f9812ac5cbd8ca79e82ae8
PaaRich <siehrichmond50@gmail.com>
2024-12-17 23:42:27 +0000

    renamed to Events and schedule

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import Layout from "./components/Layout";
 import { Route, Routes } from "react-router-dom";
-import Calendar from "./pages/Calendar";
-import Meeting from "./pages/Meeting";
 import HomePage from "./pages/HomePage";
-import Notification from "./pages/Notificaitons";
+import Events from "./pages/Events";
+import Schedule from "./pages/Schedules";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="notification" element={<Notification />} />
-        <Route path="meeting" element={<Meeting />} />
-        <Route path="calendar" element={<Calendar />} />
+        <Route path="events" element={<Events />} />
+        {/* <Route path="meeting" element={<Meeting />} /> */}
+        <Route path="schedule" element={<Schedule />} />
       </Route>
     </Routes>
   );
