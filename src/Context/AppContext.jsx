3133ec96ed12aeb50cff5b91256e6339976a9543
PaaRich <siehrichmond50@gmail.com>
2024-12-17 00:55:28 +0000

    implemented badge alert

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -4,8 +4,24 @@ import { useState } from "react";
 // eslint-disable-next-line react/prop-types
 const AppContext = ({ children }) => {
   const [open, setIsOpen] = useState(false);
+  const [meeting, setMeeting] = useState(true);
+  const [calendar, setCalendar] = useState(true);
+  const [notification, setNotification] = useState(true);
   return (
-    <Context.Provider value={{ open, setIsOpen }}>{children}</Context.Provider>
+    <Context.Provider
+      value={{
+        open,
+        setIsOpen,
+        meeting,
+        setMeeting,
+        calendar,
+        setCalendar,
+        notification,
+        setNotification,
+      }}
+    >
+      {children}
+    </Context.Provider>
   );
 };
 
