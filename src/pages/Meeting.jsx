176dd47bb71471b33657e44d78928297eb129eb1
PaaRich <siehrichmond50@gmail.com>
2024-12-17 00:56:17 +0000

    implemeted badge update

--- src/pages/Meeting.jsx
+++ src/pages/Meeting.jsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { Context } from "../Context/Context";
+
 const Meeting = () => {
-  return <div>Meeting</div>;
+  const { setMeeting } = useContext(Context);
+  return (
+    <div className="flex items-center justify-center w-full">
+      <button onClick={() => setMeeting(false)} className="border-2 ">
+        Toggle Meeting
+      </button>
+    </div>
+  );
 };
 
 export default Meeting;
--- src/pages/Notificaitons.jsx
+++ src/pages/Notificaitons.jsx
@@ -1,5 +1,15 @@
+import { useContext } from "react";
+import { Context } from "../Context/Context";
+
 const Notification = () => {
-  return <div>Notification</div>;
+  const { setNotification } = useContext(Context);
+  return (
+    <div className="flex items-center justify-center w-full">
+      <button onClick={() => setNotification(false)} className="border-2 ">
+        Notification
+      </button>
+    </div>
+  );
 };
 
 export default Notification;
