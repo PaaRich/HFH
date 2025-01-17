c4f4bea8ce84282c609d1d464843b8ff4870386e
PaaRich <siehrichmond50@gmail.com>
2025-01-17 02:21:12 +0000

    remove unwanted video element

--- src/pages/VideoPage.jsx
+++ src/pages/VideoPage.jsx
@@ -21,9 +21,6 @@ const VideoPage = () => {
           {/* <source src={videoData.asset} type="video/mp4" /> */}
           <source src={video} type="video/mp4" />
         </video>
-        {/* <video controls height="100%" width="100%">
-          <source src={video} type="video/mp4" />
-        </video> */}
         <div className="flex items-center gap-x-2 mt-2">
           <IoBookmarksOutline size={25} />
           <h1 className="text-2xl font-semibold">{videoData.title}</h1>
