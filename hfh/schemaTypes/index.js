ce07b8f56e037b3ca7570da4e7df68ad2291beb2
PaaRich <siehrichmond50@gmail.com>
2025-01-22 22:55:02 +0000

    created policy schema

--- hfh/schemaTypes/index.js
+++ hfh/schemaTypes/index.js
@@ -3,5 +3,6 @@ import category from './category'
 import post from './post'
 import author from './author'
 import training from './training'
+import policy from "./policy"
 
-export const schemaTypes = [post, author, category, blockContent, training]
+export const schemaTypes = [post, author, category, blockContent, training,policy]
---
+++ hfh/schemaTypes/policy.js
@@ -0,0 +1,49 @@
+import { defineField, defineType } from 'sanity';
+
+export default defineType({
+  name: 'policy',
+  title: 'Policy',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'blockContent',
+    })
+  ],
+
+  // preview: {
+  //   select: {
+  //     title: 'title',
+  //   },
+  //   prepare(selection) {
+  //     const {author} = selection
+  //     return {...selection, subtitle: author && `by ${author}`}
+  //   },
+  // },
+})
