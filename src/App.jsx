5f2ac4524bf29482e67b94c5b2c47f0052cd251f
PaaRich <siehrichmond50@gmail.com>
2025-02-03 00:26:57 +0000

    Routing policy

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,29 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        
         <Route index element={<HomePage />} />
         <Route path="events" element={<Events />} />
         <Route path="events/*" element={<Post />} />
-        {/* <Route path="meeting" element={<Meeting />} /> */}
         <Route path="schedule" element={<Schedule />} />
         <Route path="training" element={<Training />} />
         <Route path="training/*" element={<VideoPage />} />
-        <Route path="policy" element={<Policy />} />
-        {/* <Route
-          path="*"
-          element={<h1 className="text-3xl font-bold">Error Handling</h1>}
-        /> */}
+
+        {/* Policy routing */}
+        <Route path="policy" element={<Policy />} >
+          <Route index element={<h1>First Element</h1>}/>
+          <Route path="Employment and contractual Terms" element={<h1>Employment and contractual Terms</h1>}/>
+          <Route path="Compensation and Benefits" element={<h1>Compensation and Benefits</h1>}/>
+          <Route path="Workplace Policies and Code of Conduct" element={<h1>Workplace Policies and Code of Conduct</h1>}/>
+          <Route path="Health and Safety Regulations" element={<h1>Health and Safety Regulations</h1>}/>
+          <Route path="Training and Professional Development" element={<h1>Training and Professional Development</h1>}/>
+          <Route path="Work Schedule and Leave Policies" element={<h1>Work Schedule and Leave Policies</h1>}/>
+          <Route path="Performance Evaluation and Discipline" element={<h1>Performance Evaluation and Discipline</h1>}/>
+          <Route path="Confidentiality and Data Protection" element={<h1>Confidentiality and Data Protection</h1>}/>
+          <Route path="Workplace Diversity and Inclusion" element={<h1>Workplace Diversity and Inclusion</h1>}/>
+          <Route path="Employee Support and Well-being" element={<h1>Employee Support and Well-being</h1>}/>
+        </Route>
+
       </Route>
     </Routes>
   );
