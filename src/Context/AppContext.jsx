927b54a79f672d5b7e26699d7414e4cf3e01e883
PaaRich <siehrichmond50@gmail.com>
2025-02-07 01:10:11 +0000

    deleted myEventList Array

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,52 +5,10 @@ import { useState } from "react";
 const AppContext = ({ children }) => {
   const [post, setPost] = useState(null);
   const [open, setIsOpen] = useState(false);
-  const [addEvent, setAddEvent] = useState(false);
+  const [displayAddEvent, setDisplayAddEvent] = useState(false);
   const [position, setPosition] = useState({ top: 0, left: 0 });
-  const [myEventList, setMyEventList] = useState([
-    {
-      title: "Team Meeting",
-      start: new Date(2024, 11, 20, 10, 0), // December 20, 2024, 10:00 AM
-      end: new Date(2024, 11, 20, 11, 30), // December 20, 2024, 11:30 AM
-      allDay: false,
-    },
-    {
-      title: "Project Development",
-      start: new Date(2024, 11, 18, 10, 0), // December 20, 2024, 10:00 AM
-      end: new Date(2024, 11, 18, 11, 30), // December 20, 2024, 11:30 AM
-      allDay: false,
-    },
-    {
-      title: "Lunch Break",
-      start: new Date(2024, 11, 20, 13, 0), // December 20, 2024, 1:00 PM
-      end: new Date(2024, 11, 20, 14, 0), // December 20, 2024, 2:00 PM
-      allDay: false,
-    },
-    {
-      title: "Annual Company Party",
-      start: new Date(2024, 11, 22, 18, 0), // December 22, 2024, 6:00 PM
-      end: new Date(2024, 11, 22, 23, 0), // December 22, 2024, 11:00 PM
-      allDay: false,
-    },
-    {
-      title: "Christmas Holiday",
-      start: new Date(2024, 11, 25), // December 25, 2024
-      end: new Date(2024, 11, 26), // December 26, 2024
-      allDay: true,
-    },
-    {
-      title: "Project Deadline",
-      start: new Date(2024, 11, 30, 17, 0), // December 30, 2024, 5:00 PM
-      end: new Date(2024, 11, 30, 23, 59), // December 30, 2024, 11:59 PM
-      allDay: false,
-    },
-    {
-      title: "New Year's Day",
-      start: new Date(2025, 0, 1), // January 1, 2025
-      end: new Date(2025, 0, 1), // January 1, 2025
-      allDay: true,
-    },
-  ]);
+
+  const [myEventList, setMyEventList] = useState([]);
 
   return (
     <Context.Provider
@@ -59,8 +17,8 @@ const AppContext = ({ children }) => {
         setPost,
         open,
         setIsOpen,
-        addEvent,
-        setAddEvent,
+        displayAddEvent,
+        setDisplayAddEvent,
         myEventList,
         setMyEventList,
         position,
