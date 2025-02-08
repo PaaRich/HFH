20f90ae6bc08125e3bb2237885e20b41de165675
PaaRich <siehrichmond50@gmail.com>
2025-02-08 01:21:35 +0000

    app

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -1,14 +1,45 @@
 import { Context } from "./Context";
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import client from "../client"
 
 // eslint-disable-next-line react/prop-types
-const AppContext = ({ children }) => {
+  const AppContext = ({ children }) => {
   const [post, setPost] = useState(null);
   const [open, setIsOpen] = useState(false);
   const [displayAddEvent, setDisplayAddEvent] = useState(false);
   const [position, setPosition] = useState({ top: 0, left: 0 });
-
   const [myEventList, setMyEventList] = useState([]);
+
+  useEffect(() => {
+    const event = `*[_type == "event"] {
+   _id,
+    title,
+    start,
+    end,
+    allday
+}`;
+
+    // Fetch data using GROQ
+      // Function to fetch data
+      const fetchEvent = async () => {
+        try {
+          const response = await client.fetch(event);
+          // if (!response) {
+          //   throw new Error('Network response was not ok');
+          // }
+          // const result = await response.json();
+          setMyEventList(response);
+        } catch (err) {
+          console.log(err)
+        } finally {
+          console.log("fetched")
+        }
+    }
+    fetchEvent();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  
 
   return (
     <Context.Provider
